9a01b3d2bda2832ec94a12ae12db1003187da086
marc durst <marcdurst@MacBook-Pro-de-marc.local>
2018-07-19 02:28:29 +0200

    cd now handles /absolute/paths, ~/fromhome and ./relative/paths

--- srcs/cd.c
+++ srcs/cd.c
@@ -38,24 +38,66 @@ static void	cd_error(int no, char *arg)
 static int	is_dir(char *path)
 {
 	struct stat s;
-	
+
 	stat(path, &s);
 	if (s.st_mode & S_IFDIR)
 		return (1);
 	return (0);
 }
 
+static char	*prev_cwd(char *path)
+{
+	char buf[4096];
+	int i;
+
+	i = 0;
+	getcwd(buf, 4096);
+	while (buf[i])
+		i++;
+	while (buf[i - 1] && buf[i - 1] != '/')
+		buf[i -- - 1] = '\0';
+	return (ft_strjoin(buf, &path[3]));
+}
+
+static char	*parse_path(char *path)
+{
+	char *ret;
+	int	i;
+
+	i = 0;
+	if (path[0] == '.' && path[1] != '.')
+		ret = ft_strjoin(get_var_content("PWD"), &path[1]);
+	else if (path[0] == '~')
+		ret = ft_strjoin(get_var_content("HOME"), &path[1]);
+	else if (path[0] == '.' && path[1] == '.')
+		ret = prev_cwd(path);
+	else
+		ret = ft_strdup(path);
+	while (ret[i])
+		i++;
+	while (ret[i - 1] && ret[i - 1] == '/' && i > 1)
+		ret[i-- - 1] = '\0';
+	return (ret);
+}
+
 int	cd_builtin(int ac, char **av)
 {
+	char	*parsed;
+
 	if (ac == 1)
 		change_cwd(NULL);
-	else if (!ft_strcmp(av[1], "--"))
-		change_cwd(get_var_content("OLDPWD"));
-	else if (access(av[1], F_OK) == -1)
-		cd_error(EXIST_ERROR, av[1]);
-	else if (!is_dir(av[1]))
-		cd_error(TYPE_ERROR, av[1]);
 	else
-		change_cwd(av[1]);
+	{
+		parsed = parse_path(av[1]);
+		if (!ft_strcmp(av[1], "--"))
+			change_cwd(get_var_content("OLDPWD"));
+		else if (access(parsed, F_OK) == -1)
+			cd_error(EXIST_ERROR, av[1]);
+		else if (!is_dir(parsed))
+			cd_error(TYPE_ERROR, av[1]);
+		else
+			change_cwd(parsed);
+		ft_strdel(&parsed);
+	}
 	return (1);
 }
